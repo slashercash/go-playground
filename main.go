2a6eb03a5ab2fc9bd1163c79988c3b4be3ef26c6
David Zweili <david.zweili@scaling.ch>
2022-09-09 15:07:49 +0200

    Add Struct example

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name         string
+	FailedLogins int
+	Locked       bool
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	myUser := User{
+		Name: "admin",
+	}
+
+	fmt.Println(myUser)
+
+	myUser.FailedLogins += 1
+
+	if myUser.FailedLogins > 0 {
+		myUser.Locked = true
+	}
+
+	fmt.Println(myUser)
 }
